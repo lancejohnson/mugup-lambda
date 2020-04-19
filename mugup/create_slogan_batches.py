c8b666dda3ff250aab595fe2b4e4e902fd73bb1a
RichStone <richard.had@hotmail.de>
2020-04-19 20:59:56 +0200

    this might be the appropriate functions architecture for our use case

---
+++ mugup/create_slogan_batches.py
@@ -0,0 +1,5 @@
+# should be the entry point of our application
+# will get the csv from s3
+# batch it into dicts and pass to the next function
+# they are doing something very similar by the way:
+# https://docs.aws.amazon.com/pinpoint/latest/developerguide/tutorials-importing-data-lambda-function-input-split.html
---
+++ mugup/render_and_upload.py
@@ -0,0 +1,1 @@
+# here, with the slogan dicts, the images would be rendered and uploaded
